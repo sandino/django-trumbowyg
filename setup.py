b36870d729845301682516338138491e015e8aea
sandino <vladts77@gmail.com>
2023-04-04 18:42:00 +0300

    move meta info to pyproject.toml

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-trumbowyg',
-    version='2.0.0',
+    version='2.0.1',
     packages=['trumbowyg'],
     include_package_data=True,
     install_requires=[],
